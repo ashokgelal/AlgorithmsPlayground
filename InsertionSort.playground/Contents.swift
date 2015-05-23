30a066d0da9fc14afb76d28c4548c5206aa5dcdd
Ashok Gelal <ashokgelal@gmail.com>
2015-05-23 15:26:29 -0600

    Implemented Insertion Sort

---
+++ InsertionSort.playground/Contents.swift
@@ -0,0 +1,57 @@
+// ### INSERTION SORT ###
+
+import Foundation
+
+func sort(data: [Int]) -> [Int] {
+    if data.count < 1 {
+        return data
+    }
+    // create a copy to avoid changing original data
+    var input = NSMutableArray(array: data) as NSArray as! [Int]
+    
+    func swapAtIndex(indx: Int, withIndex: Int) {
+        if indx == withIndex {
+            return
+        }
+        var temp = input[indx]
+        input[indx] = input[withIndex]
+        input[withIndex] = temp
+    }
+    
+    for var i = 0; i < input.count; i++ {
+        var minIndex = i
+        for var j = i + 1; j < input.count; j++ {
+            if input[j] < input[minIndex] {
+                minIndex = j
+            }
+        }
+        swapAtIndex(i, minIndex)
+    }
+    
+    return input
+}
+
+func sequenceEquals(arr1: [Int], arr2: [Int]) -> Bool {
+    if arr1.count < arr2.count {
+        return false
+    }
+    var match = true
+    for (index, _) in enumerate(arr1) {
+        if arr1[index] != arr2[index] {
+            match = false
+            break
+        }
+    }
+    return match
+}
+
+let data = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48]
+let answer = [2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
+
+let sorted = sort(data)
+sequenceEquals(answer, sorted)
+
+
+// O(1) extra space (ignoring input copy)
+// O(n^2) comparisons and swaps
+// ideal when data is nearly sorted or the problem size is small
